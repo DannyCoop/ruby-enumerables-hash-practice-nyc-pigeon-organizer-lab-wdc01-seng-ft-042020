6aba5c98bebf3a7b0c768c325e9063438bd5e9ae
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-17 09:47:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  solution = {}
+  data.each do |data, val|
+    val.each do |info, names|
+      names.each do |name|
+        solution[name] ||={}
+        solution[name][data] ||= []
+        solution[name][data].push(info.to_s)
+      end
+    end
+  end
+  solution
 end
